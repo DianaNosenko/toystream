a88058c41f65f13d5b663f039a1407083b12ddda
Diana <diana.nosenko1205@gmail.com>
2023-02-27 18:13:12 +0000

    sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -4,7 +4,17 @@ import styles from './Sidebar.module.css'
 const Sidebar = () => {
     return (
         <div className={styles.sidebarStyle}>
-            Sidebar 
+            <div className={styles.logoImage}>
+                <img src="/logo.svg" alt="no image:(" />
+            </div>
+            <div className={styles.icons}>
+                <a href="#"><img src="/icons/Pinterest.svg" alt="" /></a>
+                <a href="#"><img src="/icons/Behance.svg" alt="" /></a>
+                <a href="#"><img src="/icons/Dribbble.svg" alt="" /></a>
+                <a href="#"><img src="/icons/Snapchat.svg" alt="" /></a>
+                <a href="#"><img src="/icons/Linkedin.svg" alt="" /></a>
+                <a href="#"><img src="/icons/Facebook.svg" alt="" /></a>
+            </div>
         </div>
     );
 }
