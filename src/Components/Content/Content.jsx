2165f76f24862a8b21f8c296abf3fe5e393a78bb
Diana <diana.nosenko1205@gmail.com>
2023-03-13 12:38:57 +0000

    create routes

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import styles from './Content.module.css' 
+import Home from './Home/Home'
+import About from './About/About'
+import Games from './Games/Games'
+import Career from './Career/Career'
+import News from './News/News'
+import Contact from './Contact/Contact'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 const Content = () => {
     return (
-        <div className={styles.contentStyle}>
-            Content
-        </div>
+        <BrowserRouter>
+            <div className={styles.contentStyle}>
+                <Routes>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/about-us" element={<About/>}/>
+                    <Route path="/games" element={<Games/>}/>
+                    <Route path="/career" element={<Career/>}/>
+                    <Route path="/news" element={<News/>}/>
+                    <Route path="/contact-us" element={<Contact/>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
