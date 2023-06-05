d42df8d3a8d821cdfa875ceca0f4b4cec36d3270
Diana <diana.nosenko1205@gmail.com>
2023-06-05 23:07:33 +0100

    add scrolling through the screen on About page

--- src/Components/Content/About/About.jsx
+++ src/Components/Content/About/About.jsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import styles from './About.module.scss' 
 import Company from './Sections/Company';
 import Team from './Sections/Team';
+import { Element, Link } from 'react-scroll';
 
 
 const About = () => {
@@ -10,8 +11,20 @@ const About = () => {
 
     return (
         <div className={styles.aboutWrap}>
-            <Company/>
-            <Team/>
+            <div className={styles.buttons}>
+                <Link to="company" smooth={true} duration={500}>
+                    <button>company</button>
+                </Link>
+                <Link to="team" smooth={true} duration={500}>
+                    <button>team</button>
+                </Link>
+            </div>
+            <Element name="company" className="section">
+                <Company/>
+            </Element>
+            <Element name="team" className="section">
+                <Team/>
+            </Element>
         </div>
     );
 }
