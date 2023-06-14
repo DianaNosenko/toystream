63b0261509628a52ab233b684264c9dd77848719
Diana <diana.nosenko1205@gmail.com>
2023-06-14 14:52:54 +0100

    add scrolling with wheel on About page

--- src/Components/Content/About/About.jsx
+++ src/Components/Content/About/About.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import styles from './About.module.scss' 
 import Company from './Sections/Company';
 import Team from './Sections/Team';
+import styles from './About.module.scss' 
 import { Element, Link } from 'react-scroll';
+import { FullPage, Slide } from 'react-full-page';
 
 
 const About = () => {
     const {t} = useTranslation();
-
-    return (
+    return(
         <div className={styles.aboutWrap}>
             <div className={styles.buttons}>
                 <Link to="company" smooth={true} duration={500}>
-                    <button>company</button>
+                    <a>{t('About_button')} <span className="square"></span></a>
                 </Link>
                 <Link to="team" smooth={true} duration={500}>
-                    <button>team</button>
+                    <a>{t('Team_button')}<span className="square"></span></a>
                 </Link>
             </div>
+            <FullPage>
+            <Slide>
             <Element name="company" className="section">
                 <Company/>
             </Element>
+            </Slide>
+            <Slide>
             <Element name="team" className="section">
                 <Team/>
             </Element>
+            </Slide>
+            </FullPage>
         </div>
+
     );
-}
+};
+
 export default About;
