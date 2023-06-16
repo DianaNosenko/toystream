8f4262d341b3d4d5b578604638115a8444eee499
Diana <diana.nosenko1205@gmail.com>
2023-06-16 18:42:29 +0100

    fixed font in Slider component

--- src/Components/Content/Games/Games.jsx
+++ src/Components/Content/Games/Games.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import GamesMain from './Sections/GamesMain';
 import OurWorks from './Sections/OurWorks';
 import LastProjects from './Sections/LastProjects';
-import About from '../About/About'
 
 const Games = () => {
     return (
